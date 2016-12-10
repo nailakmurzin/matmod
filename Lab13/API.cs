fd015c2886833ed74e071b9c11799f0b6940a3db
Наиль <@NAIL-PC>
2016-12-11 02:02:35 +0500

    Добавьте файлы проекта.

---
+++ Lab13/API.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Troschuetz;
+using Troschuetz.Random;
+
+namespace Lab13
+{
+    static class API
+    {
+        public static float GetZ1(double l0, double o, double w)
+        {
+            double a1 = Math.Sin(l0 * o) / Math.Pow(Math.Cos(o), 1 / l0);
+            double b1 = Math.Pow(Math.Cos((l0 - 1) * o) / w, (1 - l0) / l0);
+            return (float)(a1 * b1);
+        }
+        public static float GetZ2(double l1, double o, double w)
+        {
+            double a2 = Math.Sin(l1 * o) / Math.Pow(Math.Cos(o), 1 / l1);
+            double b = Math.Cos((l1 - 1) * o) / w;
+            double bb = (1 - l1) / l1;
+            double b2 = Math.Pow(b, bb);
+            return (float)(a2 * b2);
+        }
+        public static Tuple<float[], float[]> GetZ1Z2(int n, double l0, double l1)
+        {
+            Random rnd = new Random();
+            var z1 = new float[n];
+            var z2 = new float[n];
+            var u1 = new double[n];
+            var u2 = new double[n];
+            for (int i = 0; i < n; ++i)
+            {
+                u1[i] = rnd.NextDouble();
+                u2[i] = rnd.NextDouble();
+            }
+            double al0 = 1 / l0;
+            double al1 = 1 / l1;
+            Parallel.For(0, n, (i) =>
+            {
+                double o = Math.PI * (u1[i] - 0.5d), w = -Math.Log(u2[i]);
+
+                double a1 = Math.Sin(l0 * o) / Math.Pow(Math.Cos(o), al0);
+                double b1 = Math.Pow(Math.Cos((l0 - 1) * o) / w, (1 - l0) * al0);
+
+                double a2 = Math.Sin(l1 * o) / Math.Pow(Math.Cos(o), al1);
+                double b = Math.Cos((l1 - 1) * o) / w;
+                double b2 = Math.Pow(b, (1 - l1) * al1);
+                z1[i] = (float)(a1 * b1);
+                z2[i] = (float)(a2 * b2);
+            });
+            //for (int i = 0; i < n; ++i)
+            //{
+            //    double o = Math.PI * (u1[i] - 0.5d), w = Math.Log(u2[i]);
+
+            //    double a1 = Math.Sin(l0 * o) / Math.Pow(Math.Cos(o), al0);
+            //    double b1 = Math.Pow(Math.Cos((l0 - 1) * o) / w, (1 - l0) * al0);
+
+            //    double a2 = Math.Sin(l1 * o) / Math.Pow(Math.Cos(o), al1);
+            //    double b = Math.Cos((l1 - 1) * o) / w;
+            //    double b2 = Math.Pow(b, (1 - l1) * al1);
+            //    z[i] = new Z1Z2 { Z1 = (float)(a1 * b1), Z2 = (float)(a2 * b2) };
+            //}
+            return new Tuple<float[], float[]>(z1, z2);
+        }
+        public static float GetZ2Sample(int n, double l1)
+        {
+            Random rnd = new Random();
+            float z2 = 0f;
+            var u1 = new double[n];
+            var u2 = new double[n];
+            for (int i = 0; i < n; ++i)
+            {
+                u1[i] = rnd.NextDouble();
+                u2[i] = rnd.NextDouble();
+            }
+            for (int i = 0; i < n; ++i)
+            {
+                double o = Math.PI * (u1[i] - 0.5d), w = -Math.Log(u2[i]);
+                z2 += GetZ2(l1, o, w);
+            }
+            return z2;
+        }
+
+        public static TRandom TRAND = new TRandom();
+        static void Main1(string[] args)
+        {
+            int T = 2000, N = 1000000;
+            double[] Z = new double[N];
+
+            //for (int i = 0; i < N; ++i)
+            //{
+            //    double t = 0, x = 0;
+            //    int k = 0;
+            //    while (t < 2000)
+            //    {
+            //        t += TRAND.Exponential(1d);
+            //        k++;
+            //    }
+
+            //    for (int j = 0; j < k; ++j)
+            //    {
+            //        x += TRAND.Normal(0, 1);
+            //    }
+            //    Z[i] = x;
+            //}
+
+            //var text = new StringBuilder();
+            //for (int i = 0; i < N; ++i)
+            //{
+            //    text.AppendLine(Z[i].ToString());
+            //}
+            //File.WriteAllText(@"C:\Nail\WriteText2.txt", text.ToString());
+
+        }
+    }
+}
---
+++ Lab13/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace Lab13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbEx2 = new System.Windows.Forms.PictureBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.pb1 = new System.Windows.Forms.PictureBox();
+            this.tabControl2 = new System.Windows.Forms.TabControl();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.tabPage6 = new System.Windows.Forms.TabPage();
+            this.pb2 = new System.Windows.Forms.PictureBox();
+            this.pbEx1 = new System.Windows.Forms.PictureBox();
+            this.pbEx3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbEx2)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).BeginInit();
+            this.tabControl2.SuspendLayout();
+            this.tabPage5.SuspendLayout();
+            this.tabPage6.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbEx1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbEx3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbEx2
+            // 
+            this.pbEx2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbEx2.Location = new System.Drawing.Point(3, 3);
+            this.pbEx2.Name = "pbEx2";
+            this.pbEx2.Size = new System.Drawing.Size(558, 354);
+            this.pbEx2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pbEx2.TabIndex = 0;
+            this.pbEx2.TabStop = false;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(572, 386);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.tabControl2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(564, 360);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Генератор";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.pbEx1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(564, 360);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Пример1";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.pbEx2);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(564, 360);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Пример2";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.pbEx3);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(564, 360);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Пример3";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // pb1
+            // 
+            this.pb1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pb1.Location = new System.Drawing.Point(3, 3);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(544, 322);
+            this.pb1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pb1.TabIndex = 0;
+            this.pb1.TabStop = false;
+            // 
+            // tabControl2
+            // 
+            this.tabControl2.Controls.Add(this.tabPage5);
+            this.tabControl2.Controls.Add(this.tabPage6);
+            this.tabControl2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl2.Location = new System.Drawing.Point(3, 3);
+            this.tabControl2.Name = "tabControl2";
+            this.tabControl2.SelectedIndex = 0;
+            this.tabControl2.Size = new System.Drawing.Size(558, 354);
+            this.tabControl2.TabIndex = 1;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Controls.Add(this.pb1);
+            this.tabPage5.Location = new System.Drawing.Point(4, 22);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage5.Size = new System.Drawing.Size(550, 328);
+            this.tabPage5.TabIndex = 0;
+            this.tabPage5.Text = "tabPage5";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // tabPage6
+            // 
+            this.tabPage6.Controls.Add(this.pb2);
+            this.tabPage6.Location = new System.Drawing.Point(4, 22);
+            this.tabPage6.Name = "tabPage6";
+            this.tabPage6.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage6.Size = new System.Drawing.Size(550, 328);
+            this.tabPage6.TabIndex = 1;
+            this.tabPage6.Text = "tabPage6";
+            this.tabPage6.UseVisualStyleBackColor = true;
+            // 
+            // pb2
+            // 
+            this.pb2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pb2.Location = new System.Drawing.Point(3, 3);
+            this.pb2.Name = "pb2";
+            this.pb2.Size = new System.Drawing.Size(544, 322);
+            this.pb2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pb2.TabIndex = 1;
+            this.pb2.TabStop = false;
+            // 
+            // pbEx1
+            // 
+            this.pbEx1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbEx1.Location = new System.Drawing.Point(3, 3);
+            this.pbEx1.Name = "pbEx1";
+            this.pbEx1.Size = new System.Drawing.Size(558, 354);
+            this.pbEx1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pbEx1.TabIndex = 1;
+            this.pbEx1.TabStop = false;
+            // 
+            // pbEx3
+            // 
+            this.pbEx3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbEx3.Location = new System.Drawing.Point(3, 3);
+            this.pbEx3.Name = "pbEx3";
+            this.pbEx3.Size = new System.Drawing.Size(558, 354);
+            this.pbEx3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pbEx3.TabIndex = 1;
+            this.pbEx3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(572, 386);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbEx2)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).EndInit();
+            this.tabControl2.ResumeLayout(false);
+            this.tabPage5.ResumeLayout(false);
+            this.tabPage6.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbEx1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbEx3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbEx2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.PictureBox pb1;
+        private System.Windows.Forms.TabControl tabControl2;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.TabPage tabPage6;
+        private System.Windows.Forms.PictureBox pb2;
+        private System.Windows.Forms.PictureBox pbEx1;
+        private System.Windows.Forms.PictureBox pbEx3;
+    }
+}
+
